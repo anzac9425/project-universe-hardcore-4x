8b9ec81aa7197b3573d9919fce6640f32c7d92c8
anzac9425 <anzac94251@gmail.com>
2026-03-22 18:15:57 +0900

    Tablet-202603221815

--- skripts/map/data/galaxy_data.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name GalaxyData
-
-var galaxy_seed: int
-var systems: Array[SystemData] = []
--- skripts/scenes/Ingame/ingame.gd
+++ skripts/scenes/Ingame/ingame.gd
@@ -1,12 +1,1 @@
-extends Node
-
-func _ready():
-
-	if not GameSession.game_started:
-		return
-
-	var system = GameSession.get_random_system()
-	if system == null:
-		return
-
-	GameSession.enter_system(system)
+extends Node2D
