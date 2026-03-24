06bc62af3504214209c3392ffb0d81b44b9530de
anzac9425_debut <anzac94251@gmail.com>
2026-03-24 12:22:25 +0900

    202603241222

--- skripts/core/scene_manager.gd
+++ skripts/core/scene_manager.gd
@@ -1,1 +1,87 @@
 extends Node
+
+const ERR_NOT_INITED := "ERR_SCENEMANAGER_NOT_INITIALIZED"
+const ERR_SCENE_NOT_EXISTS := "ERR_SCENE_NOT_EXISTS"
+const ERR_LOAD_FAILED := "ERR_SCENE_LOAD_FAILED"
+
+const LOADING_SCENE_PATH := "res://scenes/Loading.tscn"
+
+var container: Node
+var current_scene: Node
+var target_scene_path: String
+
+var loading_progress: float = 0.0
+var is_loading := false
+
+
+func initialize(scene_container: Node):
+	container = scene_container
+
+
+func change_scene(path: String) -> void:
+
+	if container == null:
+		push_error(tr(ERR_NOT_INITED))
+		return
+
+	if not ResourceLoader.exists(path):
+		push_error("%s: %s" % [tr(ERR_SCENE_NOT_EXISTS), path])
+		return
+
+	var packed: PackedScene = load(path)
+
+	if packed == null:
+		push_error("%s: %s" % [tr(ERR_LOAD_FAILED), path])
+		return
+
+	_replace_scene(packed)
+
+
+func change_scene_with_loading(path: String):
+
+	if container == null:
+		push_error(tr(ERR_NOT_INITED))
+		return
+
+	if not ResourceLoader.exists(path):
+		push_error("%s: %s" % [tr(ERR_SCENE_NOT_EXISTS), path])
+		return
+
+	target_scene_path = path
+	loading_progress = 0.0
+	is_loading = true
+
+	var packed: PackedScene = load(LOADING_SCENE_PATH)
+
+	if packed == null:
+		is_loading = false
+		push_error("%s: %s" % [tr(ERR_LOAD_FAILED), LOADING_SCENE_PATH])
+		return
+
+	_replace_scene(packed)
+
+
+func _replace_scene(packed: PackedScene) -> void:
+
+	if current_scene:
+		current_scene.queue_free()
+		current_scene = null
+
+	var scene := packed.instantiate()
+
+	container.add_child(scene)
+
+	current_scene = scene
+
+
+func finish_loading(packed: PackedScene) -> void:
+
+	if packed == null:
+		is_loading = false
+		push_error("%s: %s" % [tr(ERR_LOAD_FAILED), target_scene_path])
+		return
+
+	is_loading = false
+	loading_progress = 1.0
+
+	_replace_scene(packed)
--- skripts/scenes/Main/main.gd
+++ skripts/scenes/Main/main.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@onready var active_scene: Node = $ActiveScene
+
+
+func _ready() -> void:
+	SceneManager.initialize(active_scene)
+	SceneManager.change_scene("res://scenes/MainMenu.tscn")
